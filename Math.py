3339bcac3d82ab2ed8e138cd8a9d8215bcfae82a
adityabindroo <adityabindroo@gmail.com>
2021-05-28 14:45:44 +0530

    Adeded implemation

--- Math.py
+++ Math.py
@@ -1,8 +1,15 @@
+#Add implementation
 def add(x,y):
     pass
+	
+#Sub implementation	
 def subtract(x,y):
     pass
+	
+#Mul implementation	
 def multiply(x,y):
     pass
+	
+#div implementation	
 def divide(x,y):
     pass
